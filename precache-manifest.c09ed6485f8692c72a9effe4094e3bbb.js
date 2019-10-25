e20e9add9c0dfd1206122a064ad603bed3ac84a5
Jayesh Mann <36336898+jayeshmann@users.noreply.github.com>
2019-10-26 00:53:38 +0530

    added a react app

---
+++ precache-manifest.c09ed6485f8692c72a9effe4094e3bbb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b70089ffa09c29da66702863b2abd4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5e1a0d81d1deddf1d5d",
+    "url": "/static/js/2.1b5676aa.chunk.js"
+  },
+  {
+    "revision": "9bc5f7f2e209acb926ec",
+    "url": "/static/js/main.a3ca3012.chunk.js"
+  },
+  {
+    "revision": "d7b12fec78261b4870c7",
+    "url": "/static/js/runtime~main.65602910.js"
+  }
+]);
